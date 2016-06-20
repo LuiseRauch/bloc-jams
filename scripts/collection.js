9ee5ebe1e87b4efca02c7781e8573ae118b5b2cf
Luise Rauch <luise.rauch@gmail.com>
2016-06-20 16:13:36 +0200

    Checkpoint 31 jQuery Play/Pause: completed

--- scripts/collection.js
+++ scripts/collection.js
@@ -18,7 +18,7 @@ var buildCollectionItemTemplate = function() {
 };
 
 $(window).load(function() {
-  var $collectionContainer = $('album-covers');
+  var $collectionContainer = $('.album-covers');
   $collectionContainer.empty();
 
   for (var i = 0; i < 12; i++) {
